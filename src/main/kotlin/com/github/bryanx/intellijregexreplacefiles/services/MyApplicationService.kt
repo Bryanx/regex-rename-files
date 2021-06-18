1e22f171776b1964737934355ef6d9328eef85da
GitHub Action <action@github.com>
2021-06-18 08:31:43 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/bryanx/intellijregexreplacefiles/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.bryanx.intellijregexreplacefiles.services
+
+import com.github.bryanx.intellijregexreplacefiles.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
