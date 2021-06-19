b620e0cd66460fb47abf02216aa8a0ec60849753
Bryan de Ridder <br.deridder@gmail.com>
2021-06-19 16:41:46 +0200

    Make sure renaming is undo-able

--- src/main/kotlin/com/github/bryanx/intellijregexrenamefiles/ReplaceFileNamesAction.kt
+++ src/main/kotlin/com/github/bryanx/intellijregexrenamefiles/ReplaceFileNamesAction.kt
@@ -2,7 +2,7 @@ package com.github.bryanx.intellijregexrenamefiles
 
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.PlatformDataKeys
-import com.intellij.openapi.application.ApplicationManager
+import com.intellij.openapi.command.WriteCommandAction
 import com.intellij.openapi.project.DumbAwareAction
 import org.jetbrains.annotations.NotNull
 
@@ -19,14 +19,16 @@ class ReplaceFileNamesAction : DumbAwareAction() {
 
     override fun actionPerformed(event: AnActionEvent) {
         val selectedFiles = event.getData(PlatformDataKeys.VIRTUAL_FILE_ARRAY)
+        val currentProject = event.getData(PlatformDataKeys.PROJECT)
         val dialog = ReplaceFileNamesDialogWrapper(selectedFiles)
         if (dialog.showAndGet()) {
-            selectedFiles?.forEach { file ->
-                ApplicationManager.getApplication().runWriteAction {
-                    val newName = createNewFileName(dialog, file.name)
-                    file.rename(this, newName)
-                }
-            }
+            error("test")
+//            WriteCommandAction.runWriteCommandAction(currentProject, dialog.title, "ProjectViewPopupMenu", {
+//                selectedFiles?.forEach { file ->
+//                    val newName = createNewFileName(dialog, file.name)
+//                    file.rename(this, newName)
+//                }
+//            })
         }
     }
 
