ff207c6f8e4ebfd852609f96f92a2ddf14820628
Vladislav Rassokhin <vladislav.rassokhin@jetbrains.com>
2023-03-11 18:23:27 +0100

    Improve rename action:
    * run preparation under read action
    * do validation in read action
    * show all validation errors if any
    * only actual rename is done in write action
    * don't do anything if new name is same as old one

--- src/main/kotlin/nl/bryanderidder/regexrenamefiles/ReplaceFileNamesAction.kt
+++ src/main/kotlin/nl/bryanderidder/regexrenamefiles/ReplaceFileNamesAction.kt
@@ -2,14 +2,15 @@ package nl.bryanderidder.regexrenamefiles
 
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.PlatformDataKeys
+import com.intellij.openapi.application.ReadAction
 import com.intellij.openapi.application.invokeLater
 import com.intellij.openapi.command.WriteCommandAction
 import com.intellij.openapi.diagnostic.Logger
 import com.intellij.openapi.project.DumbAwareAction
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.ui.Messages.showErrorDialog
+import com.intellij.openapi.util.text.StringUtil
 import nl.bryanderidder.regexrenamefiles.icons.RegexRenameFilesIcons.ActionIcon
-import org.jetbrains.annotations.NotNull
 import java.io.IOException
 
 /**
@@ -17,55 +18,98 @@ import java.io.IOException
  */
 class ReplaceFileNamesAction : DumbAwareAction(ActionIcon) {
 
-    private var changesList = mutableListOf<RenameEvent>()
-
     override fun update(event: AnActionEvent) {
         val selectedFiles = event.getData(PlatformDataKeys.VIRTUAL_FILE_ARRAY)
         event.presentation.isEnabledAndVisible = event.project != null &&
-                selectedFiles != null &&
-                (selectedFiles.size > 1 || selectedFiles.isNotEmpty() && selectedFiles[0].children.isNotEmpty())
+                selectedFiles != null && when (selectedFiles.size) {
+            0 -> false
+            1 -> selectedFiles.first().children?.isNotEmpty() ?: false
+            else -> true
+        }
     }
 
     override fun actionPerformed(event: AnActionEvent) {
-        val selectedFiles = event.getData(PlatformDataKeys.VIRTUAL_FILE_ARRAY)?.toList() ?: listOf()
-        val project = event.project
+        val selectedFiles = event.getData(PlatformDataKeys.VIRTUAL_FILE_ARRAY)?.toList() ?: return
+        val project = event.project ?: return
         val dialog = ReplaceFileNamesDialogWrapper(selectedFiles)
-        val groupId = "ProjectViewPopupMenu"
+        val groupId = "ReplaceFileNamesAction"
         if (dialog.showAndGet()) {
-            WriteCommandAction.runWriteCommandAction(
-                project, dialog.title, groupId,
-                {
-                    renameFiles(dialog, project)
-                }
-            )
+            val events: List<RenameEvent> = ReadAction.compute<List<RenameEvent>, Throwable> {
+                prepareFilesRename(dialog, project)
+            } ?: return
+            WriteCommandAction.runWriteCommandAction(project, ReplaceFileNamesDialogWrapper.TITLE, groupId, {
+                renameFiles(project, events)
+            })
         }
     }
 
-    private fun renameFiles(dialog: ReplaceFileNamesDialogWrapper, project: Project?) {
-        lateinit var renameEvent: RenameEvent
-        try {
-            dialog.getFiles().forEach { file ->
-                renameEvent = RenameEvent(file, file.name, createNewFileName(dialog, file.name))
-                file.rename(this, renameEvent.newName)
-                changesList.add(renameEvent)
-            }
-        } catch (e: IOException) {
-            val errorMessage = "File '${renameEvent.newName}' already exists " +
-                    "in directory '${renameEvent.file.parent.path}'"
-            LOGGER.warn(errorMessage, e)
-            revertRenameFiles()
+    private fun prepareFilesRename(dialog: ReplaceFileNamesDialogWrapper, project: Project): List<RenameEvent>? {
+        val events = dialog.getFiles().mapNotNull { file ->
+            val oldName = file.name
+            val newName = createNewFileName(dialog, oldName)
+            if (newName != oldName) RenameEvent(file, oldName, newName) else null
+        }
+        if (events.isEmpty()) return null
+        val errors = validate(events)
+        if (errors.isNotEmpty()) {
+            val errorMessage = "Cannot rename ${errors.size} ${StringUtil.pluralize("file", errors.size)}:\n${errors.joinToString("\n")}"
+            LOGGER.warn(errorMessage)
             invokeLater {
-                showErrorDialog(project, errorMessage, dialog.title)
+                showErrorDialog(project, errorMessage, ReplaceFileNamesDialogWrapper.TITLE)
             }
+            return null
         }
-        changesList.clear()
+        return events
     }
 
-    private fun revertRenameFiles() = changesList.forEach {
+    private fun renameFiles(project: Project, events: List<RenameEvent>) {
+        val performedEvents = ArrayList<RenameEvent>()
+        for (renameEvent in events) {
+            try {
+                renameEvent.file.rename(this, renameEvent.newName)
+                performedEvents.add(renameEvent)
+            } catch (e: IOException) {
+                val errorMessage = "Cannot rename file '${renameEvent.file.name}', rolled back other renames. Error: ${e.message}"
+                LOGGER.warn(errorMessage, e)
+                revertRenameFiles(performedEvents)
+                invokeLater {
+                    showErrorDialog(project, errorMessage, ReplaceFileNamesDialogWrapper.TITLE)
+                }
+                return
+            }
+        }
+    }
+
+    private fun validate(events: List<RenameEvent>): List<String> {
+        val errors = ArrayList<String>()
+        for (fs in events.map { it.file.fileSystem }.toSet()) {
+            if (fs.isReadOnly) {
+                errors.add("File system containing one of files is read only")
+            }
+        }
+        for (it in events) {
+            if (!it.file.isValid) {
+                errors.add("File '${it.file.name}' is not valid one")
+                continue
+            }
+            if (!it.file.fileSystem.isValidName(it.newName)) {
+                errors.add("New file name '${it.newName}' is not supported by file system")
+                continue
+            }
+            val parent = it.file.parent
+            if (parent?.findChild(it.newName) != null) {
+                errors.add("New name '${it.newName}' for '${it.oldName}' already used in directory '${parent.path}'")
+                continue
+            }
+        }
+        return errors
+    }
+
+    private fun revertRenameFiles(performedEvents: List<RenameEvent>) = performedEvents.forEach {
         it.file.rename(this, it.oldName)
     }
 
-    private fun createNewFileName(dialog: ReplaceFileNamesDialogWrapper, oldName: @NotNull String): String {
+    private fun createNewFileName(dialog: ReplaceFileNamesDialogWrapper, oldName: String): String {
         val fromText = dialog.getReplaceFromText()
         val toText = dialog.getReplaceToText()
         return when {
